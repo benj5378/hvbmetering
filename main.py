e2b802d3a089815db0ced7222172fe6f0a17804a
Benjamin Bræstrup Sayoc <benj5378@outlook.com>
2024-06-06 15:06:43 +0200

    Introduce microcontroller script

---
+++ main.py
@@ -0,0 +1,180 @@
+from machine import Pin
+import utime
+import network
+import ntptime
+import machine
+import requests
+import mcron
+import _thread
+import socket
+import gc
+import io
+import os
+# import ssl
+
+blueLED = Pin(45, Pin.OUT)
+redLED = Pin(46, Pin.OUT)
+redLED.value(0)  # On
+selfIp = ""
+
+lastPostTimeSeconds = utime.time()
+meters = [{"pin": 8}, {"pin": 9}]
+kwhpercount = 1/1000
+
+
+def connectToInternet():
+    global selfIp
+    global nic
+    nic.active(True)
+    while not nic.isconnected():
+        #print("tryign")
+        try:
+            nic.connect("Hedelands Veteranbane", "")
+        except Exception:
+            utime.sleep(5)
+            continue
+    utime.sleep(5)
+    selfIp = nic.ifconfig()[0]
+    print("Ip: " + selfIp)
+
+
+def syncTime():
+    ntptime.settime()
+    # Fix timezone
+    t = utime.localtime()
+    machine.RTC().datetime((t[0], t[1], t[2], t[6] + 1, t[3] + 1, t[4], t[5], 0))
+    print("Time: " + str(utime.localtime()))
+
+
+def postRecent(callbackId, currentTime, callbackMemory):
+    #utime.sleep(0.1)
+    global lastPostTimeSeconds
+    global meters
+    lastPostTime = utime.localtime(lastPostTimeSeconds)
+    start = f"{lastPostTime[0]}-{lastPostTime[1]}-{lastPostTime[2]} {lastPostTime[3]}:{lastPostTime[4]}:{lastPostTime[5]}"
+    currentTimeSeconds = utime.time()
+    currentTime = utime.localtime(currentTimeSeconds)
+    end = f"{currentTime[0]}-{currentTime[1]}-{currentTime[2]} {currentTime[3]}:{currentTime[4]}:{currentTime[5]}"
+    #durationHours = utime.ticks_diff(lastPostTimeTicks, currentTimeTicks) / 60 / 60
+    for meter in meters:
+        kwh = kwhpercount * meter["count"]
+        meter["count"] = 0
+        checkConnection()
+        try:
+            response = requests.post(
+                "https://forbrug.ibk.dk/receiver.php",
+                data=f"start={start}&end={end}&kwh={kwh}&meter={meter["pin"]}",
+                headers=dict({"Content-Type": "application/x-www-form-urlencoded"})
+            )
+        except Exception as e:
+            print(e)
+        #print(str(utime.localtime()) + " Response " + str(response.status_code) + ": " + str(response.content))
+    lastPostTimeSeconds = currentTimeSeconds
+
+
+def blink():
+    blueLED.value(0)  # On
+    utime.sleep(0.2)
+    blueLED.value(1)  # Off
+
+
+def counting(meter):
+    countPin = Pin(meter["pin"], Pin.IN, Pin.PULL_DOWN)
+    justCounted = False
+    lastTime = utime.ticks_ms()
+    while True:
+        utime.sleep(0.03)
+        if countPin.value() == 1 and not justCounted:
+            meter["count"] = meter["count"] + 1
+            duration = utime.ticks_diff(utime.ticks_ms(), lastTime)
+            #                kwhpercount * count * hours = kw
+            meter["kW"] = kwhpercount * 1 / (duration / 1000 / 60 / 60)
+            lastTime = utime.ticks_ms()
+            justCounted = True
+            #print(f"Meter pin {meter["pin"]}: count {meter["count"]}, duration {duration / 1000} s, {meter["kW"]} kW")
+            _thread.start_new_thread(blink, ())
+        elif countPin.value() == 0 and justCounted:
+            #print("duraaaation: " + str(utime.ticks_ms() - lastTime))
+            justCounted = False
+
+
+def printTime(callbackId, currentTime, callbackMemory):
+    print(utime.localtime())
+
+
+def serverListener():
+    s = socket.socket()
+    s.bind((selfIp, 11500))
+    s.listen()
+    #s.setblocking(False)
+    while True:
+        try:
+            conn, addr = s.accept()
+            conn.settimeout(1)
+            conn.recv(1024)
+            conn.send("HTTP/1.1 200 OK\n")
+            conn.send("Cache-Control: no-store\n\n")
+            json = "[\n"
+            for meter in meters:
+                json = json + f'    {{"meter": {meter["pin"]}, "kW": {meter["kW"]}}},\n'
+            json = json + "]"
+            conn.send(json)
+            conn.close()
+        except Exception as e:
+            #print(e)
+            continue
+
+
+def checkConnection():
+    if not nic.isconnected():
+        connectToInternet()
+
+
+
+
+gc.enable()
+
+
+#logfile = open("log.txt", "a")
+#os.dupterm(logfile)
+
+class logToFile(io.IOBase):
+    def __init__(self):
+        pass
+
+    def write(self, data):
+        with open("logfile.txt", mode="a") as f:
+            f.write(data)
+        return len(data)
+
+os.dupterm(logToFile())
+print("Logging started")
+
+
+nic = network.WLAN(network.STA_IF)
+connectToInternet()
+syncTime()
+redLED.value(1)  # Off
+
+
+for meter in meters:
+    meter["count"] = 0
+    meter["kW"] = 0
+    _thread.start_new_thread(counting, (meter,))
+
+
+# If postRecent called directly, crash happens
+def annoyingBug(callbackId, currentTime, callbackMemory):
+    _thread.start_new_thread(postRecent, (callbackId, currentTime, callbackMemory))
+
+mcron.init_timer()
+mcron.insert(mcron.PERIOD_HOUR, range(0, mcron.PERIOD_HOUR, mcron.PERIOD_HOUR // 4), "every15min", annoyingBug)
+# Exceptions are TLPTimeException, see documentation
+
+
+while True:
+    try:
+        checkConnection()
+        serverListener()
+    except Exception as e:
+        print(e)
